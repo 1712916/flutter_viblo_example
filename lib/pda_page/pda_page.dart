d981d00bd0c1b49076637f5b7b63eb33626f3655
Ngo Tan Vinh <>
2023-04-07 15:27:29 +0700

    listen scan pda

--- lib/pda_page/pda_page.dart
+++ lib/pda_page/pda_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class PDAPage extends StatefulWidget {
   const PDAPage({Key? key}) : super(key: key);
@@ -8,14 +9,48 @@ class PDAPage extends StatefulWidget {
 }
 
 class _PDAPageState extends State<PDAPage> {
+  final FocusNode focusNode = FocusNode();
+
+  String _chars = '';
+
   @override
   Widget build(BuildContext context) {
+    focusNode.requestFocus();
     return Scaffold(
       appBar: AppBar(title: Text('PDA')),
-      body: Column(
-        children: [
-          TextFormField(),
-        ],
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          _chars = '';
+          setState(() {});
+        },
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              RawKeyboardListener(
+                autofocus: true,
+                focusNode: focusNode,
+                onKey: (RawKeyEvent event) {
+                  print(event.runtimeType);
+                  print('event.logicalKey.keyLabel: ${event.logicalKey.keyLabel}');
+                  print('event.data.keyLabel: ${event.data.keyLabel}');
+
+                  if (event is RawKeyDownEvent) {
+                    if (event.logicalKey.keyLabel.length == 1) {
+                      setState(() {
+                        _chars += '${event.logicalKey.keyLabel.characters.first}';
+                      });
+                    }
+                  } else {}
+                },
+                child: Text("a: ${_chars}", style: TextStyle(fontSize: 30, color: Colors.black)),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
