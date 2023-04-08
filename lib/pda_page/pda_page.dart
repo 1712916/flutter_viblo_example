1fcad7f401fa463ffde29539fed9e19c11633d62
1712916 <>
2023-04-08 23:57:35 +0700

    update logic detect scan one time and multitime

--- lib/pda_page/pda_page.dart
+++ lib/pda_page/pda_page.dart
@@ -1,5 +1,9 @@
+import 'dart:async';
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:rxdart/rxdart.dart';
 
 class PDAPage extends StatefulWidget {
   const PDAPage({Key? key}) : super(key: key);
@@ -9,49 +13,112 @@ class PDAPage extends StatefulWidget {
 }
 
 class _PDAPageState extends State<PDAPage> {
+  final ValueNotifier<String> codeNotifier = ValueNotifier('');
+
+  @override
+  void dispose() {
+    codeNotifier.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: PDAWidget(
+        onChanged: (code) {
+          codeNotifier.value = code;
+        },
+        onNotFound: () {
+          codeNotifier.value = 'not found';
+        },
+        child: ValueListenableBuilder<String>(
+          valueListenable: codeNotifier,
+          builder: (context, value, child) {
+            return Center(child: Text('Code: \n$value'));
+          },
+        ),
+      ),
+    );
+  }
+}
+
+class PDAWidget extends StatefulWidget {
+  const PDAWidget({Key? key, this.onChanged, this.onNotFound, this.child}) : super(key: key);
+
+  final ValueChanged<String>? onChanged;
+  final VoidCallback? onNotFound;
+  final Widget? child;
+
+  @override
+  State<PDAWidget> createState() => _PDAWidgetState();
+}
+
+class _PDAWidgetState extends State<PDAWidget> {
+  static const String _notFound = '404_PDA_SCAN_NOT_FOUND';
+
+  final BehaviorSubject<String> _subject = BehaviorSubject();
+
+  late final StreamSubscription _streamSubscription;
+
   final FocusNode focusNode = FocusNode();
 
-  String _chars = '';
+  final StringBuffer _chars = StringBuffer();
+
+  @override
+  void initState() {
+    super.initState();
+    _streamSubscription = _subject.stream.debounceTime(const Duration(milliseconds: 100)).listen((code) {
+      if (code == _notFound) {
+        log('[PDA SCAN LOG]: $_notFound');
+        widget.onNotFound?.call();
+      } else {
+        log('[PDA SCAN LOG]: $code');
+        widget.onChanged?.call(code);
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    _streamSubscription.cancel();
+    _subject.close();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     focusNode.requestFocus();
-    return Scaffold(
-      appBar: AppBar(title: Text('PDA')),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          _chars = '';
-          setState(() {});
-        },
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              RawKeyboardListener(
-                autofocus: true,
-                focusNode: focusNode,
-                onKey: (RawKeyEvent event) {
-                  print(event.runtimeType);
-                  print('event.logicalKey.keyLabel: ${event.logicalKey.keyLabel}');
-                  print('event.data.keyLabel: ${event.data.keyLabel}');
-
-                  if (event is RawKeyDownEvent) {
-                    if (event.logicalKey.keyLabel.length == 1) {
-                      setState(() {
-                        _chars += '${event.logicalKey.keyLabel.characters.first}';
-                      });
-                    }
-                  } else {}
-                },
-                child: Text("a: ${_chars}", style: TextStyle(fontSize: 30, color: Colors.black)),
-              )
-            ],
-          ),
-        ),
-      ),
+    return KeyboardListener(
+      autofocus: true,
+      focusNode: focusNode,
+      onKeyEvent: (KeyEvent event) {
+        switch (event.runtimeType) {
+          case KeyDownEvent:
+            {
+              if (event.logicalKey.keyLabel.length == 1) {
+                _chars.write(event.logicalKey.keyLabel.characters.first);
+                _subject.add(_chars.toString());
+              }
+              return;
+            }
+          case KeyUpEvent:
+            {
+              switch (event.logicalKey.keyLabel.toUpperCase()) {
+                case 'F12':
+                  {
+                    _subject.add(_notFound);
+                    return;
+                  }
+                case 'ENTER':
+                  {
+                    _chars.clear();
+                    return;
+                  }
+              }
+            }
+        }
+      },
+      child: widget.child ?? const SizedBox(),
     );
   }
 }
